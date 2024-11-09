9b1b47f99516d4b553cd65b84dd2e0558b60ba94
KirillMoz <kirill.mozzhegorov@yandex.ru>
2024-11-10 01:56:16 +0500

    Решение задачи 14.3

---
+++ WorkShop14ex1/Contact.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop14ex1
+{
+    internal class Contact
+    {
+        private string name;
+        private string surname;
+        private long phone;
+        private string email;
+        public Contact(string Name, string Surname, long Phone, string Email) 
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Phone = Phone;
+            this.Email = Email;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        { 
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public long Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
---
+++ WorkShop14ex1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop14ex1
+{
+    internal class Program
+    {
+        static void PrintList(List<Contact> ListContact)
+        {
+            foreach (Contact Contact in ListContact)
+                Console.WriteLine($"{Contact.Name} {Contact.Surname} {Contact.Phone} {Contact.Email}");            
+        }
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            List<Contact> phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            Console.WriteLine("Исходный список контактов");
+            PrintList(phoneBook);
+
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Cписок контактов отсортированный по имени (по-возрастанию)");
+            var sortedContactsByName = from s in phoneBook orderby s.Name select s;
+            foreach (var Contact in sortedContactsByName)
+                Console.WriteLine($"{Contact.Name} {Contact.Surname} {Contact.Phone} {Contact.Email}");
+
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Cписок контактов отсортированный по фамилии (по-возрастанию)");
+            var sortedContactsBySurname = from s in phoneBook orderby s.Surname select s;
+            foreach (var Contact in sortedContactsBySurname)
+                Console.WriteLine($"{Contact.Name} {Contact.Surname} {Contact.Phone} {Contact.Email}");
+
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Cписок контактов отсортированный по имени и фамилии (по-возрастанию)");
+            var sortedContactsByNameSurname = from s in phoneBook orderby s.Name, s.Surname select s;
+            foreach (var Contact in sortedContactsByNameSurname)
+                Console.WriteLine($"{Contact.Name} {Contact.Surname} {Contact.Phone} {Contact.Email}");
+            Console.ReadKey();
+        }
+    }
+}
